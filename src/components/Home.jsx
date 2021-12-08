315b63dcf2f561c27f7c7ad0eadf8101b88dd7db
Olzhas11Dev <olzhasdev11@gmail.com>
2021-12-08 16:58:26 +1300

    additional routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,22 @@ function Home() {
   return (
     <div className="App">
       <div className="containerd-flex justify-content-center " style={{ marginTop: '10rem' }}>
-        <div className="d-flex justify-content-center ">
-          <Link className="w-25 rounded border" to="/shop">
-            <img className="img-fluid   " src="img/food.png" style={{ cursor: 'pointer' }} alt="" />
+        <div className="row d-flex justify-content-center">
+          <Link className=" col-lg-3  col-md-4 col-sm-6 col-5 " to="/shop">
+            <img
+              className="img-fluid  max-width: 100% height: auto "
+              src="img/food.png"
+              style={{ cursor: 'pointer' }}
+              alt=""
+            />
           </Link>
-          <Link className="w-25 rounded border" to="/addItems">
-            <img className="img-fluid  " src="img/shop.png" style={{ cursor: 'pointer' }} alt="" />
+          <Link className=" col-lg-3  col-md-4 col-sm-6 col-5 " to="/addItems">
+            <img
+              className="img-fluid max-width: 100% height: auto "
+              src="img/shop.png"
+              style={{ cursor: 'pointer' }}
+              alt=""
+            />
           </Link>
         </div>
       </div>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Shop() {
   const [data, setData] = React.useState([]);
@@ -36,7 +37,6 @@ function Shop() {
   };
 
   const hanleRenove = async (elem) => {
-    console.log(elem.id);
     await Axios.delete(`https://61a71b7b8395690017be94e1.mockapi.io/basketData/${elem.id}`);
 
     getBasketProduct();
@@ -44,6 +44,15 @@ function Shop() {
 
   return (
     <div className="container">
+      <div className=" d-flex justify-content-between mt-3  ">
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <h5 className="text-dark">Main Page</h5>
+        </Link>
+        <Link to="/addItems" style={{ textDecoration: 'none' }}>
+          <h5 className="text-dark">Add Items</h5>
+        </Link>
+      </div>
+
       <div className="row">
         <div className="col-lg-9 order-1 order-lg-0 order-md-1 ">
           <div className="row">
@@ -54,7 +63,7 @@ function Shop() {
                 <div
                   onClick={() => addToBasket(e)}
                   style={{ cursor: 'pointer' }}
-                  className="card item col-lg-3 col-md-4 col-sm-6 col-6"
+                  className="card item col-lg-3 col-md-4 col-sm-6 col-6 "
                   key={e.id}>
                   <img className="img-fluid max-width: 100% height: auto" src={e.img} alt="" />
                 </div>
